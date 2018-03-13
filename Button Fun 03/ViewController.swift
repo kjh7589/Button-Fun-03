03bf56155dd51edb1ba033c64d75cab042d38b4b
D7703_25 <d7703_25@D7703-25ui-iMac.local>
2018-03-13 15:36:38 +0900

    add count

--- Button Fun 03/ViewController.swift
+++ Button Fun 03/ViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         view.backgroundColor = UIColor.yellow
     }
 
+    @IBAction func ButtonPressed(_ sender: Any) {
+        count = count + 1
+        print("\(count) Button Pressed !")
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
